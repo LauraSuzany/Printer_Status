bc98e66ea4105f50a97cbb9f63b193cd25208a89
LauraVoxData <laurasuzany1705@gmail.com>
2022-08-08 18:40:33 -0300

    Status de impressoras térmicas

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using ESCPOS_NET;
+using ESCPOS_NET.Emitters;
+
+namespace VoxDataThermalPrinterPaperStatus
+{
+    class Program
+    {
+       {
+        static SerialPrinter printer;
+        static ICommandEmitter e;
+        static void StatusChanged(object sender, EventArgs ps)
+
+        {
+            var status = (PrinterStatus)ps;
+            Console.WriteLine($"Low Paper? {status.IsPaperLow}");
+            Console.WriteLine($"Cover Open? {status.IsCoverOpen}");
+            Console.WriteLine("________________________________");
+            Console.WriteLine("             Update             ");
+            Console.WriteLine("________________________________");
+        }
+        static void setUp()
+        {
+            printer.Write(e.EnableAutomaticStatusBack());
+            printer.StatusChanged += StatusChanged;
+        }
+        static void Main(string[] args)
+        {
+
+            printer = new SerialPrinter(portName: "COM5", int.Parse("115200"));
+            e = new EPSON();
+            printer.StartMonitoring();
+            setUp();
+        }
+    }
+}
